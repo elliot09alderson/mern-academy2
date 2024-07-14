afb40e2cab17d04dacd14dede67d2bd42a21c16e
Abhishek Verma <speedlikeelectron@gmail.com>
2024-07-14 18:04:49 +0530

    ASD

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import "./app.scss"
+const App = () => {
+  return <div>
+
+<section>Hero</section>
+<section>PARALLAX</section>
+<section>Services</section>
+<section>Portfolio1</section>
+<section>Portfolio2</section>
+<section>Portfolio3</section>
+<section>Contact</section>
+
+
+  </div>;
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+    import React from 'react'
+    
+    const Navbar = () => {
+      return (
+        <div className='navbar'>
+
+
+        <div className='wrapper'>
+
+            <span>MERN ACADEMY</span>
+            <div className="socials">
+                <a href=""></a>
+            </div>
+        </div>
+        </div>
+      )
+    }
+    
+    export default Navbar
